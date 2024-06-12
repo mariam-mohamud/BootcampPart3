8502f137b3c8d1cb3d84416308038e1798a0f2ba
mariam.mohamud <mariam.mohamuddd@gmail.com>
2024-06-12 17:11:57 +0100

    getting-user-input

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Bootcamp;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.Write("Enter your name: ");
+		String name = Console.ReadLine();
+		Console.WriteLine("Hello " + name);
+	}
+	
+}
